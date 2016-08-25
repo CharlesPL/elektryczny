f281831592022983105fc0a17f193ae919661481
CharlesPL <kwcharllie379@gmail.com>
2016-08-25 16:00:56 +0200

    Add gulpfile.js, package.json and update README.md

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # elektryczny
 phpbb theme fork
 theme created for private purposes
+
+# To help us build the theme all you have to do is follow these simple steps.
+
+1. Install nodejs and npm.
+2. Go to the root folder an type in terminal 'npm install'.
+3. Type gulp and by default it will watch for all changes you have made in a repo and autocompile .scss files to pure .css
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+/**
+ * [Automate ]
+ * @type {[type]}
+ */
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+// Here you can specify the output style of .css file
+// as like the other options
+var Options = {
+    SassOutputStyle: {
+        outputStyle: 'compressed'
+    } //nested, expanded, compact, compressed (gulp-sass)
+
+};
+
+// Styles Tasks
+// Compile scss to css
+gulp.task('styles', function() {
+
+    console.log(Options.SassOutputStyle);
+    return gulp.src('theme/stylesheet.scss')
+        .pipe(sass(Options.SassOutputStyle).on('error', sass.logError))
+        .pipe(gulp.dest('theme'))
+
+});
+gulp.task('watch', function() {
+    gulp.watch('theme/styles/*.scss', ['styles']);
+
+});
+gulp.task('default', ['styles', 'watch']);
